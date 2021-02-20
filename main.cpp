609c7490c64496dc6558934a4b335b888b4d69a2
adjsky <igorlfmartins@mail.ru>
2021-02-20 12:34:21 +1000

    Add greeting variation depending on passed argument

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!\n";
+void printHello(const std::string& name) {
+    std::cout << "Hello, " << name << "!\n";
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cout << "Usage example: " << argv[0] << " %name\n";
+        return 0;
+    }
+    printHello(argv[1]);
+   
     return 0;
 }
