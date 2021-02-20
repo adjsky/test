6831ae180dbcc0b1fcc65f039b84e7dbc5d9a97e
adjsky <igorlfmartins@mail.ru>
2021-02-20 12:55:32 +1000

    Add greeting style variation

--- main.cpp
+++ main.cpp
@@ -1,16 +1,25 @@
 #include <iostream>
 #include <string>
 
-void printHello(const std::string& name) {
-    std::cout << "Hello, " << name << "!\n";
+void printHello(const std::string& name, int style) {
+    switch (style) {
+        case 1:
+            std::cout << "Greetings, " << name << "!\n";
+            break;
+        case 2:
+            std::cout << "Hello, " << name << "!\n";
+            break;
+        default:
+            std::cout << "Wrong style\n";
+    }
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        std::cout << "Usage example: " << argv[0] << " %name\n";
+    if (argc != 3) {
+        std::cout << "Usage example: " << argv[0] << " %name %style(number from 1 to 2)\n";
         return 0;
     }
-    printHello(argv[1]);
+    printHello(argv[1], std::atoi(argv[2]));
    
     return 0;
 }
